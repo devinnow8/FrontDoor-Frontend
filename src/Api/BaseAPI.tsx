d8b9041e786cb1e1eac05a7228dedf6ec0c7deb7
devInnow8 <devInnow8@gmail.com>
2023-04-13 18:13:48 +0530

    changes

--- src/Api/BaseAPI.tsx
+++ src/Api/BaseAPI.tsx
@@ -1,28 +1,24 @@
-import axios from 'axios';
+import axios from "axios";
 
-let baseUrl = 'http://localhost:3000/';
+let baseUrl = "https://frontdoor-be-2ct3.onrender.com/";
 const response = (data: any, error: any) => ({ data, error });
 
 async function post(endpointURL: any, data: any, config: any) {
-	try {
-		const serverResponse = await axios.post(
-			`${baseUrl}${endpointURL}`,
-			data,
-			config
-		);
-		return response(serverResponse, '');
-	} catch (error: any) {
-		return response('', error.message);
-	}
+  try {
+    const serverResponse = await axios.post(`${baseUrl}${endpointURL}`, data, config);
+    return response(serverResponse, "");
+  } catch (error: any) {
+    return response("", error.message);
+  }
 }
 
 async function get(endpointURL: any, config: any) {
-	try {
-		const serverResponse = await axios.get(`${baseUrl}${endpointURL}`, config);
-		return response(serverResponse, '');
-	} catch (error: any) {
-		return response('', error.message);
-	}
+  try {
+    const serverResponse = await axios.get(`${baseUrl}${endpointURL}`, config);
+    return response(serverResponse, "");
+  } catch (error: any) {
+    return response("", error.message);
+  }
 }
 
 export default { post, get };
--- src/background.ts
+++ src/background.ts
@@ -1,63 +1,64 @@
 chrome.runtime.onInstalled.addListener(() => {
-	chrome.contextMenus.create({
-		id: 'highlight',
-		title: 'Frontdoor Smart Search',
-		contexts: ['selection'],
-		enabled: false,
-		visible: false,
-	});
+  console.log("hereeee");
+
+  chrome.contextMenus.create({
+    id: "highlight",
+    title: "Frontdoor Smart Search",
+    contexts: ["selection"],
+    enabled: false,
+    visible: false,
+  });
 });
 
 const addTooltipDiv = () => {
-	const selection = window.getSelection();
-	if (selection && selection.rangeCount > 0) {
-		const range = selection.getRangeAt(0);
-		const boundingRect = range.getBoundingClientRect();
-		const message = {
-			type: 'selectedText',
-			text: selection.toString(),
-			position: {
-				top: boundingRect.top + window.pageYOffset,
-				left: boundingRect.left + window.pageXOffset,
-			},
-		};
+  const selection = window.getSelection();
+  if (selection && selection.rangeCount > 0) {
+    const range = selection.getRangeAt(0);
+    const boundingRect = range.getBoundingClientRect();
+    const message = {
+      type: "selectedText",
+      text: selection.toString(),
+      position: {
+        top: boundingRect.top + window.pageYOffset,
+        left: boundingRect.left + window.pageXOffset,
+      },
+    };
 
-		let tooltipElement: HTMLElement;
+    let tooltipElement: HTMLElement;
 
-		// Create a new tooltip element
-		tooltipElement = document.createElement('div');
-		tooltipElement.id = 'tmp-chrome-ext';
-		tooltipElement.style.position = 'absolute';
-		tooltipElement.style.top =
-			message.position.top - tooltipElement.offsetHeight + 25 + 'px';
-		tooltipElement.style.left = message.position.left + 'px';
-		tooltipElement.style.zIndex = '9999';
+    // Create a new tooltip element
+    tooltipElement = document.createElement("div");
+    tooltipElement.id = "tmp-chrome-ext";
+    tooltipElement.style.position = "absolute";
+    tooltipElement.style.top = message.position.top - tooltipElement.offsetHeight + 25 + "px";
+    tooltipElement.style.left = message.position.left + "px";
+    tooltipElement.style.zIndex = "9999";
 
-		// Add the tooltip element to the document
-		document.body.prepend(tooltipElement);
-	}
+    // Add the tooltip element to the document
+    document.body.prepend(tooltipElement);
+  }
 };
 
 chrome.contextMenus.onClicked.addListener(async (info) => {
-	const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+  const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
 
-	await chrome.scripting.executeScript({
-		target: { tabId: tabs[0].id || 0 },
-		func: addTooltipDiv,
-	});
-	await chrome.scripting.executeScript(
-		{
-			target: { tabId: tabs[0].id || 0 },
-			files: ['js/contentScript.js'],
-		},
-		() => {
-			const text = info.selectionText;
-			chrome.tabs.sendMessage(tabs[0].id || (0 as number), {
-				action: 'highlight',
-				text,
-			});
-		}
-	);
+  await chrome.scripting.executeScript({
+    target: { tabId: tabs[0].id || 0 },
+    func: addTooltipDiv,
+  });
+  await chrome.scripting.executeScript(
+    {
+      target: { tabId: tabs[0].id || 0 },
+      files: ["js/contentScript.js"],
+    },
+    () => {
+      const text = info.selectionText;
+      chrome.tabs.sendMessage(tabs[0].id || (0 as number), {
+        action: "highlight",
+        text,
+      });
+    }
+  );
 });
 
 export {};
